747569712aa6ff6035a60f58f8fdcbe6ee444d57
Alex Karpovich <akarpovich@b2broker.net>
2022-02-09 22:37:23 +0300

    add gitgnore folder .dist

--- dist/next-env-custom.js
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-
-//# sourceMappingURL=next-env-custom.js.map
